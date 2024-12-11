ad7af745715cd1061ff0a0bdda1dbf765a94ebf8
senatormad <senator.mad@gmail.com>
2024-12-11 03:03:10 +0100

    update build for working js
    Signed-off-by: senatormad <senator.mad@gmail.com>

--- webpack.common.js
+++ webpack.common.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -9,4 +10,16 @@ module.exports = {
     clean: true,
     filename: 'js/[name].[contenthash].js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'body',
+      templateParameters: {
+        externalScripts: `
+          <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+          <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+        `,
+      },
+    }),
+  ],
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,34 +1,18 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimize: true,
     minimizer: [
-      '...', // Extend existing minimizers (like `terser-webpack-plugin`)
+      '...', // Extend existing minimizers
       new CssMinimizerPlugin(),
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: 'body',
-      scriptLoading: 'defer',  // Ensures proper script loading
-      cdnScripts: [            // Custom field to inject external scripts
-        {
-          type: 'module',
-          src: 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js',
-        },
-        {
-          nomodule: true,
-          src: 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js',
-        },
-      ],
-    }),
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
